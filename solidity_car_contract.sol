497ddd73674e46fafe3ec80d3da5e907e463f9bb
Sawan kag <55222646+Sawan120@users.noreply.github.com>
2020-07-25 16:35:23 +0530

    Create solidity_car_contract.sol

---
+++ solidity_car_contract.sol
@@ -0,0 +1,16 @@
+pragma solidity >=0.4.17 <0.7.0;
+contract car{
+    string carOwner;
+    function car(string newOwner)public
+    {
+        carOwner=newOwner;
+    }
+    function setDetails(string newOwner)public
+    {
+        carOwner=newOwner;
+    }
+    function getDetails()public view returns(string)
+    {
+        return(carOwner);
+    }
+}
