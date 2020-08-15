826b25fe3e48e4e0f44d5e0160a992ea22859a72
Sawan kag <55222646+Sawan120@users.noreply.github.com>
2020-08-15 12:54:57 +0530

    Create TrainTicket.sol

---
+++ Examcode/TrainTicket.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.25 ;
+// Contract Addres 0x01431c55dd52ed649971f4704f573a53628b9c2a
+contract Ticket {
+    
+    string public trainName;
+    string public startPoint;
+    string public endPoint;
+    string public seatNo;
+    string public trainId;
+    string public passengerName;
+    uint public price;
+     constructor ( string memory _trainName, string memory _startPoint, string memory _endPoint ,string memory _seatNo ,string memory _trainId, uint _price, string memory  _passengerName) public {
+        passengerName = _passengerName;
+         trainName = _trainName;
+         startPoint = _startPoint;
+         endPoint = _endPoint;
+         seatNo = _seatNo; 
+         trainId = _trainId;
+         price = _price;
+      }
+    
+    
+}
