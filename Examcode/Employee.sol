257b49f3c475e8b40eee255fab2e49738acff04f
Sawan kag <55222646+Sawan120@users.noreply.github.com>
2020-08-15 13:12:38 +0530

    Update Employee.sol

--- Examcode/Employee.sol
+++ Examcode/Employee.sol
@@ -1,12 +1,12 @@
 pragma solidity >=0.4.22 <0.7.0;
-//Contract Addres 0xe811f7cf98d6f652c18bca2a24a72f82f1135371
+// Contract Addres 0x309c40ab857948ad05521677b66cc59a27dd780f
 contract Data
 {
           string public Employee_Name;
           int public  Employee_ID;
           string public Company_name;
           string public Department;
-    function Employee_Data (string employee_Name, int employee_ID,string company_name,string department )public
+    constructor (string employee_Name, int employee_ID,string company_name,string department )public
     {
        Employee_Name=employee_Name;
        Employee_ID=employee_ID;
